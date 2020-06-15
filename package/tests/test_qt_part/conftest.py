628319fe554e0b9e83f5fbd3d962b0322d5c93cb
Grzegorz Bokota <bokota+github@gmail.com>
2020-06-15 23:16:20 +0200

    skip windows test

--- package/tests/test_qt_part/conftest.py
+++ package/tests/test_qt_part/conftest.py
@@ -1,3 +1,4 @@
+import collections
 import warnings
 
 import pytest
@@ -19,7 +20,7 @@ def my_fixture(qapp):
 
 
 @pytest.fixture
-def qtbot(qtbot):
+def qtbot2(qtbot):
     """A modified qtbot fixture that makes sure no widgets have been leaked."""
     initial = QApplication.topLevelWidgets()
     yield qtbot
@@ -28,6 +29,7 @@ def qtbot(qtbot):
     # still not sure how to clean up some of the remaining vispy
     # vispy.app.backends._qt.CanvasBackendDesktop widgets...
     if any([n.__class__.__name__ != "CanvasBackendDesktop" for n in leaks]):
-        raise AssertionError(f"Widgets leaked!: {leaks}")
+        class_set = collections.Counter(x.__class__ for x in leaks)
+        raise AssertionError(f"Widgets leaked! ({len(leaks)}): {class_set}; {leaks}")
     if leaks:
         warnings.warn(f"Widgets leaked!: {leaks}")
